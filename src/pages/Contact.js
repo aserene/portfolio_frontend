07e9642bfe1c2c50165e86a99767a00f2045faba
Athena Serene <aserenehoosang@gmail.com>
2023-03-15 11:11:21 -0700

    added links to email, linkdin, github

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,10 @@ const Contact = () => {
                 {Header()}
             </div>
             <div>
-            <h1>This is the Contact page</h1>
+            <h1>Let's Connect</h1>
+            <h3><a href = "mailto: aserenehoosang@gmail.com" target="_blank" rel="noreferrer">aserenehoosang@gmail.com</a></h3>
+            <a href="https://www.linkedin.com/in/ahoosang-se/" target="_blank" rel="noreferrer"><img src="../images/linkedin.png" alt="linkedin"></img></a>
+            <a href="https://github.com/aserene" target="_blank" rel="noreferrer"><img src="../images/githubicon.png" alt="githublogo"></img></a>
             </div>
         </div>
     )
