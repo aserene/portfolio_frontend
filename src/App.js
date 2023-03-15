750a3c67bbdbe1eeeab9ad6f84173d61a93b862e
Athena Serene <aserenehoosang@gmail.com>
2023-03-15 16:48:56 -0700

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { Outlet } from 'react-router-dom';
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
             <a href="/"><h2>Home</h2></a>
             <a href="/about"><h2>About</h2></a>
             <a href="/projects"><h2>Projects</h2></a>
-            <a href="https://docs.google.com/document/d/12GM3qdE2fYkHnobHP2AQ5b_g71K6X9gAcOY5uY3D0BQ/edit?usp=sharing" target="_blank"><h2>Resume</h2></a>
+            <a href="https://docs.google.com/document/d/12GM3qdE2fYkHnobHP2AQ5b_g71K6X9gAcOY5uY3D0BQ/edit?usp=sharing" target="_blank" rel="noreferrer noopener"><h2>Resume</h2></a>
             <a href="/contact"><h2>Contact</h2></a>
         </div>
     )
