66fa2dc6fd9d363932d090051af7674fa78611b9
Athena Serene <aserenehoosang@gmail.com>
2023-03-16 07:19:23 -0700

    fixed headshot sizing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
                 {Header()}
             </div>
             <div className="home-cont">
-                <img className="headshot" src="https://i.imgur.com/YH3fMFb.png" alt="Athena HooSang"/>
+                <img id="headshot" src="https://i.imgur.com/YH3fMFb.png" alt="Athena HooSang"/>
                 <div id="intro">
                     <h1 id="name">Athena HooSang</h1>
                     <h1>I'm a Full-Stack developer based in California with a passion for engineering original and efficient digital solutions with delightful user experiences.</h1>
