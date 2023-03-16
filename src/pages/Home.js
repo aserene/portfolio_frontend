2930e61e41bd4083303330fe1b124e9811d021a0
Athena Serene <aserenehoosang@gmail.com>
2023-03-15 18:57:58 -0700

    home page is doneeeee

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,10 @@ const Home = () => {
             </div>
             <div className="home-cont">
                 <img className="headshot" src="https://i.imgur.com/YH3fMFb.png" alt="Athena HooSang"/>
-                <h1 id="name">Athena HooSang</h1>
-                <h3>Full-Stack developer based in California</h3>
+                <div id="intro">
+                    <h1 id="name">Athena HooSang</h1>
+                    <h1>I'm a Full-Stack developer based in California with a passion for engineering original and efficient digital solutions with delightful user experiences.</h1>
+                </div>
             </div>
             {Footer()}
         </div>
