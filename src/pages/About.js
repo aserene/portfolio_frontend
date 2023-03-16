6256b8cf440da3c0258c14ed8065301fc5436879
Athena Serene <aserenehoosang@gmail.com>
2023-03-15 20:04:26 -0700

    added styling for about and contact page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,11 +6,13 @@ const About = () => {
             <div>
                 {Header()}
             </div>
-            <div>
-            <h1>About</h1>
+            <div className="about-me">
+            <h1>About Me</h1>
             <h2>As a highly motivated, organized, and strategic software engineer, I strive to create human-centered designs to improve user experience with the highest efficiency while leveraging my background as a full-stack developer. My greatest strength lies in using my experience working in teams that implement React frameworks, creating original designs to deliver innovative experiences and large digital transformations for organizations.</h2>
 
-            <h2>Skilled in: JavaScript, HTML, CSS, Python, React, Vue, Node.js, Express.js, MongoDB, Mongoose, DjangoDB, PostgreSQL, RESTful routes, JSON, JQuery</h2>
+            <h2>I'm skilled in: JavaScript || HTML ||  CSS || Python || React || Vue || Node.js || Express.js || MongoDB || Mongoose || DjangoDB || PostgreSQL || RESTful routes || JSON ||JQuery</h2>
+
+            <h2>In my free time, I enjoy learning Turkish and Japanese, as well as surfing and practicing traditional hula dancing.</h2>
             </div>
             {Footer()}
         </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,11 +6,9 @@ const Contact = () => {
             <div>
                 {Header()}
             </div>
-            <div>
-            <h1>Let's Connect</h1>
-            <h3><a href = "mailto: aserenehoosang@gmail.com" target="_blank" rel="noreferrer">aserenehoosang@gmail.com</a></h3>
-            <a href="https://www.linkedin.com/in/ahoosang-se/" target="_blank" rel="noreferrer noopener"><img src="../images/linkedin.png" alt="linkedin"></img></a>
-            <a href="https://github.com/aserene" target="_blank" rel="noreferrer"><img src="../images/githubicon.png" alt="githublogo"></img></a>
+            <div className="connect-card">
+                <h1 id="lets-connect">Let's Connect!</h1>
+                <h2>Reach out to discuss potential collaborations or hiring opporunities at <a href="mailto: aserenehoosang@gmail.com" target="_blank" rel="noreferrer noopener">aserenehoosang@gmail.com</a></h2>
             </div>
             {Footer()}
         </div>
