ae1e381723f77a794acfbe0fd1821aacce73fd61
Athena Serene <aserenehoosang@gmail.com>
2023-03-30 19:06:56 -0700

    added github to model and serializer

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ const Project = (props) => {
             <h1>{project.title}</h1>
             <img src={project.img} alt={`Screenshot of ${project.title}`}/>
             <h3 id="description">Description: {project.desc}</h3>
-            <h3>Created Using</h3>
+            <h3>Technologies Used</h3>
             <h3>{project.tech}</h3>
             <a href={project.website}target="_blank" rel="noreferrer noopener" id="view"><h3>View Website</h3></a>
         </div>
